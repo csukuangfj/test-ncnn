486f161a16dc9ef7a06d2490eaba0b44c1e9cf7a
Fangjun Kuang <csukuangfj@gmail.com>
2023-05-16 11:48:56 +0800

    copy files

--- lstm/generate.py
+++ lstm/generate.py
@@ -2,22 +2,24 @@
 
 import torch
 import torch.nn as nn
+from lstm import RNN
+from scaling_converter import convert_scaled_to_non_scaled
 
 
 class Foo(nn.Module):
     def __init__(
         self,
-        input_size: int,
-        hidden_size: int,
-        proj_size: int,
     ):
         super().__init__()
-        self.lstm = nn.LSTM(
-            input_size=input_size,
-            hidden_size=hidden_size,
-            proj_size=proj_size,
-            num_layers=1,
-            batch_first=False,
+        self.model = RNN(
+            num_features=80,
+            subsampling_factor=4,
+            d_model=512,
+            rnn_hidden_size=1024,
+            dim_feedforward=2048,
+            num_encoder_layers=12,
+            aux_layer_period=0,
+            is_pnnx=True,
         )
 
     def forward(self, x, h, c):
@@ -30,20 +32,17 @@ class Foo(nn.Module):
           c:
             (num_layers, N, hidden_size)
         """
-        y, (hx, cx) = self.lstm(x, (h, c))
-        y = nn.functional.softmax(y, -1)
-        return y, hx, cx
+        #  y, (hx, cx) = self.lstm(x, (h, c))
+        #  y = nn.functional.softmax(y, -1)
+        return y, h, c
 
 
 def main():
-    input_size = 512
-    hidden_size = 1024
-    proj_size = 768
-    f = Foo(
-        input_size=input_size,
-        hidden_size=hidden_size,
-        proj_size=proj_size,
-    )
+    f = Foo()
+    convert_scaled_to_non_scaled(f, inplace=True, is_onnx=False)
+
+    print(f)
+    return
 
     N = 1
     T = 5
---
+++ lstm/scaling.py
@@ -0,0 +1,1 @@
+../conv-emformer/scaling.py
